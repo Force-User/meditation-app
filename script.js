0e4aaeb7fbab06d65326fad8e28251ac91002d0a
Anatoly Yakyshenko <anatoly.yakyshenko@gmail.com>
2020-11-11 22:57:57 +0300

    feat: add script.js

---
+++ script.js
@@ -0,0 +1,80 @@
+"use strict";
+const playerContainer = document.querySelector(".player-container");
+const playButton = playerContainer.querySelector(".play");
+const outline = playerContainer.querySelector(".moving-outline circle");
+const timeDisplay = playerContainer.querySelector(".time-display");
+const song = document.querySelector(".sound");
+const video = document.querySelector(".video-container video");
+const soundsButton = document.querySelector(".sound-picker");
+const timeSelect = document.querySelector(".time-select");
+
+const outlineLength = outline.getTotalLength();
+let fakeDuration = 60;
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
+
+timeSelect.addEventListener("click", (event) => {
+  if (event.target.closest("button").dataset.time) {
+    selectedTime(event.target);
+  }
+});
+soundsButton.addEventListener("click", (event) => {
+  const selectedButton = event.target.closest("button");
+  if (selectedButton.dataset.sound) {
+    selectSongMeditation(selectedButton);
+  }
+});
+
+playerContainer.addEventListener("click", (event) => {
+  if(event.target.classList.contains("play")) {
+    checkPlaying();
+  }
+    
+});
+function checkPlaying() {
+  if (song.paused) {
+    song.play();
+    video.play();
+    playButton.src = "image/svg/pause.svg";
+  } else {
+    song.pause();
+    video.pause();
+    playButton.src = "image/svg/play.svg";
+  }
+}
+
+song.ontimeupdate = () => {
+  let currentTime = song.currentTime;
+  let elapsed = fakeDuration - currentTime;
+  let seconds = Math.floor(elapsed % 60);
+  let minutes = Math.floor(elapsed / 60);
+  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+  outline.style.strokeDashoffset = progress;
+  timeDisplay.textContent = `${checkZeroInTime(minutes)}:${checkZeroInTime(seconds)}`;
+  if (currentTime >= fakeDuration) {
+    song.pause();
+    video.pause();
+    song.currentTime = 0;
+    playButton.src = "image/svg/play.svg";
+  }
+};
+
+function selectSongMeditation(elem) {
+  video.src = elem.dataset.video;
+  song.src = elem.dataset.sound;
+  checkPlaying();
+}
+function selectedTime(elem) {
+  fakeDuration = elem.dataset.time;
+  const seconds = Math.floor(fakeDuration % 60);
+  const minutes = Math.floor(fakeDuration / 60);
+  timeDisplay.textContent = `${checkZeroInTime(minutes)}:${checkZeroInTime(seconds)}`;
+  song.currentTime = 0;
+}
+
+function checkZeroInTime(time) {
+    if(time < 10) {
+        return "0" + time;
+    } 
+    return time;
+}
